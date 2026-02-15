1380202f5b4f412786b5ce9cf45cc2951d4787cc
Ben Betts <sutrannu@gmail.com>
2026-02-15 11:17:54 -0600

    new race

--- next-race.sh
+++ next-race.sh
@@ -6,7 +6,12 @@ LAST_SUNDAY="${LAST_RACE%.*}"
 THIS_SUNDAY="$(date -d "this Sunday" +'%F')"
 THIS_RACE="${THIS_SUNDAY}.md"
 if [ ! -f "${WD}/content/race/${THIS_RACE}" ]; then
-  cp "${WD}/content/race/${LAST_RACE}" "${WD}/content/race/${THIS_RACE}"
+  car_by="$(awk '/tune_by/{print $2}' "${WD}/content/race/${LAST_RACE}")"
+  sed -Ee 's/(^ {4}[a-z_]+:) .*/\1/' "${WD}/content/race/${LAST_RACE}" |\
+    sed -Ee 's/(car_image:).*/\1 '"$THIS_SUNDAY"'-car.png/' |\
+    sed -Ee 's/(car_by:).*/\1 '"$car_by"'/' |\
+    tee "${WD}/content/race/${THIS_RACE}"
+  # cp "${WD}/content/race/${LAST_RACE}" "${WD}/content/race/${THIS_RACE}"
   sed -i 's/'"$LAST_SUNDAY"'/'"$THIS_SUNDAY"'/' "${WD}/content/race/${THIS_RACE}"
   echo "Next race file created: ${WD}/content/race/${THIS_RACE}"
 
@@ -17,4 +22,4 @@ if [ ! -f "${WD}/content/race/${THIS_RACE}" ]; then
 else
   echo "Next race file exists"
 fi
-
+code "${WD}/content/race/${THIS_RACE}"
